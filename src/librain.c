skiselkov/librain

commit 3fc5ab7b810bda5119523de2bcf0907147800c65

Removed explicit VR enabling and added automatic VR detection.

src/librain.c
@@ -61,7 +61,6 @@ typedef enum {
 
 static bool_t	inited = B_FALSE;
 static char	*pluginpath = NULL;
-static bool_t	librain_in_VR = B_FALSE;
 
 static GLuint	screenshot_tex = 0;
 static GLuint	screenshot_fbo = 0;
@@ -145,6 +144,7 @@ static struct {
 	dr_t	rot_rate;
 	dr_t	le_temp;
 	dr_t	window_ice;
+	dr_t	VR_enabled;
 } drs;
 
 #define	RAIN_COMP_PHASE		xplm_Phase_Gauges
@@ -599,12 +599,6 @@ compute_precip(double now)
 }
 
 void
-librain_notify_VR(bool_t entering_VR)
-{
-	librain_in_VR = entering_VR;
-}
-
-void
 librain_draw_prepare(bool_t force)
 {
 	ALIGN16(mat4 mv_matrix);
@@ -635,10 +629,12 @@ librain_draw_prepare(bool_t force)
 	glGetIntegerv(GL_VIEWPORT, vp);
 	XPLMGetScreenSize(&w, &h);
 	/*
-	 * Only check the viewport only when NOT in VR. In VR, our viewport
-	 * is actually half of the full screen width (for "VR" reason...).
+	 * In VR the viewport width needs to be halved, because the viewport
+	 * is split between the left and right eye.
 	 */
-	if (!librain_in_VR && (vp[2] != w || vp[3] != h)) {
+	if (dr_geti(&drs.VR_enabled) != 0)
+		w /= 2;
+	if (vp[2] != w || vp[3] != h) {
 		memcpy(saved_vp, vp, sizeof (saved_vp));
 		glViewport(vp[0], vp[1], w, h);
 	}
@@ -874,6 +870,7 @@ librain_init(const char *the_pluginpath, const librain_glass_t *glass, size_t nu
 	fdr_find(&drs.rot_rate, "sim/flightmodel/position/R");
 	fdr_find(&drs.le_temp, "sim/weather/temperature_le_c");
 	fdr_find(&drs.window_ice, "sim/flightmodel/failures/window_ice");
+	fdr_find(&drs.VR_enabled, "sim/graphics/VR/enabled");
 
 	XPLMRegisterDrawCallback(rain_comp_cb, RAIN_COMP_PHASE,
 	    RAIN_COMP_BEFORE, NULL);

src/librain.h
@@ -60,7 +60,6 @@ void librain_fini(void);
 
 bool_t librain_reload_gl_progs(void);
 
-void librain_notify_VR(bool_t entering_VR);
 void librain_draw_prepare(bool_t force);
 void librain_draw_z_depth(const obj8_t *obj, const char **z_depth_group_ids);
 void librain_draw_exec(void);

commit eebc4e3b74f9d5dfd94a0e474428c0859f3cd477

Commit 3fc5ab7 introduced a regression on XP11.11 due to not finding the sim/graphics/VR/enabled dataref.

src/librain.c
@@ -144,6 +144,7 @@ static struct {
 	dr_t	rot_rate;
 	dr_t	le_temp;
 	dr_t	window_ice;
+	bool_t	VR_enabled_avail;
 	dr_t	VR_enabled;
 } drs;
 
@@ -632,7 +633,7 @@ librain_draw_prepare(bool_t force)
 	 * In VR the viewport width needs to be halved, because the viewport
 	 * is split between the left and right eye.
 	 */
-	if (dr_geti(&drs.VR_enabled) != 0)
+	if (drs.VR_enabled_avail && dr_geti(&drs.VR_enabled) != 0)
 		w /= 2;
 	if (vp[2] != w || vp[3] != h) {
 		memcpy(saved_vp, vp, sizeof (saved_vp));
@@ -854,6 +855,8 @@ librain_init(const char *the_pluginpath, const librain_glass_t *glass, size_t nu
 
 	pluginpath = strdup(the_pluginpath);
 
+	memset(&drs, 0, sizeof (drs));
+
 	fdr_find(&drs.panel_render_type, "sim/graphics/view/panel_render_type");
 	fdr_find(&drs.sim_time, "sim/time/total_running_time_sec");
 	fdr_find(&drs.proj_matrix, "sim/graphics/view/projection_matrix");
@@ -870,7 +873,8 @@ librain_init(const char *the_pluginpath, const librain_glass_t *glass, size_t nu
 	fdr_find(&drs.rot_rate, "sim/flightmodel/position/R");
 	fdr_find(&drs.le_temp, "sim/weather/temperature_le_c");
 	fdr_find(&drs.window_ice, "sim/flightmodel/failures/window_ice");
-	fdr_find(&drs.VR_enabled, "sim/graphics/VR/enabled");
+	if (dr_find(&drs.VR_enabled, "sim/graphics/VR/enabled"))
+		drs.VR_enabled_avail = B_TRUE;
 
 	XPLMRegisterDrawCallback(rain_comp_cb, RAIN_COMP_PHASE,
 	    RAIN_COMP_BEFORE, NULL);